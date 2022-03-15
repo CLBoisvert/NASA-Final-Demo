71f37b6f97fdc31843c163aa9e2e43c7f58d07bc
CLBoisvert <christopherboisvert.student@careerdevs.com>
2022-03-15 18:32:04 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/NASA/Final/Demo/NasaFinalDemoApplication.java
@@ -0,0 +1,15 @@
+package com.careerdevs.NASA.Final.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaFinalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaFinalDemoApplication.class, args);
+	}
+
+}
+
+//focus on the list and use tht
---
+++ src/test/java/com/careerdevs/NASA/Final/Demo/NasaFinalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.NASA.Final.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaFinalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
